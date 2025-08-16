622f3f1e4811d2abd8a764f28aea31051736ced8
Prady-SK <pradosh.shirgaonkar@gmail.com>
2025-08-16 19:03:05 +0530

    feat: added local storage for storing posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Typography } from "@mui/material";
 import { BlogPost } from "./types";
 import "./App.css";
 import BlogTable from "./components/blog-table/BlogTable";
+import blogData from "./data/blog-data";
 
 const App: React.FC = () => {
-  const [blogPosts, setBlogPosts] = useState<BlogPost[]>([
-    {
-      id: 1,
-      title: "My First Blog",
-      author: "Pradosh",
-      content: "This is the first blog content",
-      status: "Published",
-      date: "2025-08-14",
-    },
-    {
-      id: 2,
-      title: "React Tips",
-      author: "Pradosh",
-      content: "Some useful React tips",
-      status: "Draft",
-      date: "2025-08-13",
-    },
-    {
-      id: 3,
-      title: "ABC of performance",
-      author: "Shirgaonkar",
-      content: "This is the ABC of performance enhancement in React",
-      status: "Published",
-      date: "2025-08-10",
-    },
-  ]);
+  const [blogPosts, setBlogPosts] = useState<BlogPost[]>(() => {
+    const storedPosts = localStorage.getItem("localBlogData");
+    return storedPosts ? JSON.parse(storedPosts) : blogData;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("localBlogData", JSON.stringify(blogPosts));
+  }, [blogPosts]);
 
   return (
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
---
+++ src/data/blog-data.ts
@@ -0,0 +1,28 @@
+const blogData = [
+  {
+    id: 1,
+    title: "My First Blog",
+    author: "Pradosh",
+    content: "This is the first blog content",
+    status: "Published",
+    date: "2025-08-14",
+  },
+  {
+    id: 2,
+    title: "React Tips",
+    author: "Pradosh",
+    content: "Some useful React tips",
+    status: "Draft",
+    date: "2025-08-13",
+  },
+  {
+    id: 3,
+    title: "ABC of performance",
+    author: "Shirgaonkar",
+    content: "This is the ABC of performance enhancement in React",
+    status: "Published",
+    date: "2025-08-10",
+  },
+];
+
+export default blogData;
